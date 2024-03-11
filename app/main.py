bfa4235386c37a8f8141d2001d7ae24256b51716
john-e-moore <jmoore87jr@gmail.com>
2024-03-11 15:19:47 -0400

    added pages for blog and projections

--- app/main.py
+++ app/main.py
@@ -7,7 +7,16 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@main.route('/blog')
+def blog():
+    return render_template('blog.html')
+
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.username)
+
+@main.route('/projections')
+@login_required
+def projections():
+    return render_template('projections.html', name=current_user.username)
