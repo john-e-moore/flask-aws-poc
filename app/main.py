1a5d5d34714c7ed038e6eaf6ea18473fda289a93
john-e-moore <jmoore87jr@gmail.com>
2024-03-11 15:41:36 -0400

    blog posts now read from static/blog_posts and displayed on blog page

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+from markdown import markdown
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
@@ -9,7 +11,19 @@ def index():
 
 @main.route('/blog')
 def blog():
-    return render_template('blog.html')
+    # List all markdown files and sort by date, most recent first.
+    posts_dir = 'app/static/blog_posts'
+    posts_files = [f for f in os.listdir(posts_dir) if f.endswith('.md')]
+    posts_files.sort(reverse=True)
+    # Read and convert each post to HTML.
+    posts = []
+    for filename in posts_files:
+        filepath = os.path.join(posts_dir, filename)
+        with open(filepath, 'r') as file:
+            post_content = file.read()
+        html_content = markdown(post_content)
+        posts.append({'title': filename, 'content': html_content})
+    return render_template('blog.html', posts=posts)
 
 @main.route('/profile')
 @login_required
---
+++ app/static/blog_posts/2024-03-11_first-post.md
@@ -0,0 +1,3 @@
+## 2024-03-11
+# First Blog Post
+> Fall seven times, stand up eight. --Japanese proverb
